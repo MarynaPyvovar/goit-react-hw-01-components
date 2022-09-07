7827c93c14a689029b675f11b23bda20053a0a0b
MarynaPyvovar <marina18renkas@gmail.com>
2022-09-07 11:58:57 +0300

    04 markup

--- src/components/TransactionHistory/TranactionHistory.jsx
+++ src/components/TransactionHistory/TranactionHistory.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
-export const TransactionHistory = () => {
-    return (<table class="transaction-history">
+export const TransactionHistory = ({items}) => {
+    return (<table className="transaction-history">
   <thead>
     <tr>
       <th>Type</th>
@@ -9,18 +9,16 @@ export const TransactionHistory = () => {
       <th>Currency</th>
     </tr>
   </thead>
-
-  <tbody>
-    <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>
-    <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr>
+        <tbody>
+            {
+                items.map(item => 
+                    <tr key={item.id}>
+                        <td>{item.type}</td>
+                        <td>{item.amount}</td>
+                        <td>{item.currency}</td>
+                    </tr>
+                )
+            }
   </tbody>
 </table>)
 }
